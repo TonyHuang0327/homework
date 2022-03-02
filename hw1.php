5d3efde15e8d4d748a6da86e052d7a83b2758602
TonyHuang0327 <100340004+TonyHuang0327@users.noreply.github.com>
2022-03-02 21:24:12 +0800

    Add files via upload

---
+++ hw1.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+      <center><font size="7" font color="green"><b>hw1forPHP</b></font></center>
+      <br>
+      <br>
+      <br>  
+    </head>
+    <body>
+        <?php
+        $a=5;
+        $b=10;
+        echo "a+b=";
+        echo "$a+$b";
+        echo "<br>";
+        echo "<b>i am so handsome<b>\n";
+        ?>
+        </body>
+</html>
