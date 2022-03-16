b7edfd547fbaafcd96c0b733be06fc07857cdc6e
TonyHuang0327 <100340004+TonyHuang0327@users.noreply.github.com>
2022-03-16 22:40:56 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<title>墾丁三日遊</title>
+<head><center><font size ="8" color="orange"><b>墾丁三日遊</b></font></center></head>
+<body>
+    <?php
+    echo "<body bgcolor='#95FAF3'>";
+    ?>
+    <center><font size="6">1.行程內容</font></center><br>
+    <center><mark>在陽光明媚的夏天，趕快來墾丁好好鬆一下吧，現在活動期間，2人同行，1人免費，心動不如馬上行動!!!<br></mark></center>
+    <marquee>一人只要599!</marquee>
+    <center><IMG SRC=https://image.kkday.com/v2/image/get/w_960%2Cc_fit%2Cq_55%2Ct_webp/s1.kkday.com/product_6597/20191210105401_6zngz/jpg></IMG></center>
+    <center><a href="https://mimihan.tw/kenting-travel/"target="_blank"title="詳細內容">詳細內容</A></center><br>
+    <center><font size="6">2.表單提交</font></center>
+    <form>
+        <center>姓名:<input type="text"name="Uname"></center><br>
+        <center>E.mail:<input type="text"name="Umail"></center><br>
+        <center>電話:<input type="text"name="Utel"></center>
+        <center>性別:</center><br>
+        <center><input type="radio"name="gender"value="B">男<input type="radio"name="gender"value="G">女<input type="radio"name="gender"value="O">其他</center><br>
+        <center>食物偏好:</center><br>
+        <center><input type="radio"name="foodPrefrence"value="V">素食<input type="radio"name="foodPrefrence"value="M">葷食</center><br>
+        <center>衣服尺寸表</center>
+        <center><table BORDER>
+            <TR><TH>\</TH><TH>XL</TH><TH>L</TH><TH>M</TH><TH>S</TH></TR>
+            <TR><TH>長</TH><TH>140</TH><TH>120</TH><TH>100</TH><TH>80</TH></TR>
+            <TR><TH>寬</TH><TH>85</TH><TH>70</TH><TH>55</TH><TH>40</TH></TR>
+            <TR><TH>身高</TH><TH>180以上</TH><TH>170-180</TH><TH>160-170</TH><TH>150-160</TH></TR>
+        </table></center>
+        <center>衣服尺寸:</center>
+        <center><select>
+            <option>XL</option>
+	        <option>L</option>
+	        <option>M</option>
+            <option>S</option>
+        </select></center>
+        <center>衣服顏色:
+        <input type="color" name=cloth></center>
+        <center>生日:
+        <input type="date"><br>
+        <center>人數
+        <center><select>
+            <option>1</option>
+	        <option>2</option>
+	        <option>3</option>
+            <option>4</option>
+        </select></center>
+        <input type="submit"></center>
+    </form>
+    </body>
+</html>
